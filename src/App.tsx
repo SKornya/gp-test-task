01eea2b910813c0d8c76f014203a394b5f37078a
SKornya <kornyakovS@gmail.com>
2024-02-16 23:33:41 +0300

    move currency state to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import Header from './components/Header/Header';
+import { useState } from 'react';
+
 import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
+
+import Header from './components/Header/Header';
 import Content from './components/Content/Content';
 
+import './App.css';
+
+const currencies: string[] = ['$', '€', '¥'];
+
 function App() {
+  // храним тип валюты здесь, чтобы передавать его в оба дочерних компонента
+  // и не использовать контекст, редакс и т.п.
+  const [currency, setCurrency] = useState<string>(currencies[0]);
+
   return (
-    <Theme className='container' preset={presetGpnDefault}>
-      <Header />
-      <Content />
+    <Theme className="container" preset={presetGpnDefault}>
+      <Header
+        currency={currency}
+        currencies={currencies}
+        setCurrency={setCurrency}
+      />
+      <Content currency={currency} />
     </Theme>
   );
 }
